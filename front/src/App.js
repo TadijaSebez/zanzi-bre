79d027faaebb669d1922a7ac7378ec7835cc7b4b
Strahinja Sekulic <strahinja0123@gmail.com>
2024-06-03 07:55:40 +0200

    Add: Login & Register

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LoginPage } from "./pages/auth/LoginPage";
+import { RegisterPage } from './pages/auth/RegisterPage';
 
 export default function App() {
   return (
-    <div>
-    </div>
+    <main>
+        <Router>
+            <Routes>
+                <Route exact path='/' element={<LoginPage/>} />
+                <Route exact path='/register' element={<RegisterPage/>} />
+            </Routes>
+        </Router>
+    </main>
   );
 }
