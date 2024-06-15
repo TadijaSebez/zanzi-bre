3e86e3abe83b6e3289a541c241d89660f661cfab
Lazar Milanovic <lazzarmilanovic@gmail.com>
2024-06-15 08:22:33 +0200

    Add: Export to file

--- zanzibre/parser/parser.py
+++ zanzibre/parser/parser.py
@@ -21,6 +21,10 @@ def read_model(path):
         ret = f.read()
     return ret
 
+def export_dict(model_dict):
+    with open("template.json", "w") as f:
+        f.write(json.dumps(model_dict))
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Parse a DSL model using a specified grammar.')
     parser.add_argument('--grammar', type=str, required=True, help='Path to the grammar file')
@@ -30,12 +34,8 @@ if __name__=="__main__":
     model_str = read_model(args.model)
     mm = metamodel_from_file(args.grammar)
     model = mm.model_from_str(model_str)
-
-    def cname(o):
-        return o.__class__.__name__
-
-    for relation in model.relations:
-        print(relation.relationName)
-
     model_dict = model_to_dict(model)
-    print(json.dumps(model_dict))
+
+    #print(json.dumps(model_dict))
+
+    export_dict(model_dict)
