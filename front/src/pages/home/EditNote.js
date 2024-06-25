ecb242877ce2383efa50828b789e44b6af4ac196
Strahinja Sekulic <strahinja0123@gmail.com>
2024-06-25 15:43:18 +0200

    Fix: Different UI based on the premission level

--- front/src/pages/home/EditNote.js
+++ front/src/pages/home/EditNote.js
@@ -32,28 +32,27 @@ export default function EditNote({onShareCallback, onSaveCallback, onNewNoteRequ
             </div>}
             {note != undefined && <div className="card showing h-100">
                 <div className="v-spacer-s flex center gap-xs">
-                    <div className="input-wrapper regular-border w-100" style={{paddingRight:'16px', paddingLeft:'8px'}}>
+                    <div className={`input-wrapper w-100 ${note?.permission == 'viewer' ? 'disabled-border' : 'regular-border'}`} style={{paddingRight:'16px', paddingLeft:'8px'}}>
                         <span className="material-symbols-outlined icon input-icon">titlecase</span>
-                        <input placeholder="Note Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                        <input placeholder="Note Title" value={title} disabled={note?.permission == 'viewer'} onChange={(e) => setTitle(e.target.value)}/>
                     </div>
-                    <button className="surface-button gap-xs flex center" onClick={() => onDiscardCallback()}>
+                    {note?.permission != 'viewer' && <button className="surface-button gap-xs flex center" onClick={() => onDiscardCallback()}>
                         <span className="material-symbols-outlined icon small-icon">cancel</span>
                         Discard
-                    </button>
-                    <button className="surface-button gap-xs flex center" onClick={() => onShareCallback(undefined)}>
+                    </button>}
+                    {note?.noteId !== -1 && note?.permission == 'owner' && <button className="surface-button gap-xs flex center" onClick={() => onShareCallback(undefined)}>
                         <span className="material-symbols-outlined icon small-icon">share</span>
                         Share
-                    </button>
-                    <button className="solid-accent-button gap-xs flex center" style={{paddingRight:'16px'}} onClick={save}>
+                    </button>}
+                    {note?.permission != 'viewer' && <button className="solid-accent-button gap-xs flex center" style={{paddingRight:'16px'}} onClick={save}>
                         <span className="material-symbols-outlined icon small-icon">save</span>
                         Save
-                    </button>
-    
+                    </button>}
                 </div>
     
-                <div className="input-wrapper regular-border v-spacer-xs" style={{height:'calc(100% - 52px)'}}>
+                <div className={`input-wrapper v-spacer-xs ${note?.permission == 'viewer' ? 'disabled-border' : 'regular-border'}`} style={{height:'calc(100% - 52px)'}}>
                     <span className="material-symbols-outlined icon input-icon" style={{alignSelf:'start', paddingTop:'14px'}}>description</span>
-                    <textarea className="h-100" style={{padding:'16px 0 16px 12px'}} placeholder="Content" value={content} onChange={(e) => setContent(e.target.value)}/>
+                    <textarea className="h-100" style={{padding:'16px 0 16px 12px'}} placeholder="Content" disabled={note?.permission == 'viewer'} value={content} onChange={(e) => setContent(e.target.value)}/>
                 </div>
             </div>}
         </div>)
